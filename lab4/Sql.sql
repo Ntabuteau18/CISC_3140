ef5ea5b83f4a567c9ec7bcfb4c5174a4dba0fd9e
Ntabuteau18 <ntabuteau60@gmail.com>
2022-05-04 02:42:54 -0400

    changes

---
+++ lab4/Sql.sql
@@ -0,0 +1,40 @@
+.header on
+.mode csv
+
+CREATE TABLE Car_data (
+    Timestamp DATETIME,
+    Email TEXT,
+    Name TEXT,
+    Year INT,
+    Make TEXT,
+    Model TEXT,
+    Car_ID INT,
+    Judge_ID TEXT,
+    Judge_Name TEXT,
+    Racer_Turbo INT,
+    Racer_Supercharged INT,
+    Racer_Performance INT,
+    Racer_Horsepower INT,
+    Car_Overall INT,
+    Engine_Modifications INT,
+    Engine_Performance INT,
+    Engine_Chrome INT,
+    Engine_Detailing INT,
+    Engine_Cleanliness INT,
+    Body_Frame_Undercarriage INT,
+    Body_Frame_Suspension INT,
+    Body_Frame_Chrome INT,
+    Body_Frame_Detailing INT,
+    Body_Frame_Cleanliness INT,
+    Mods_Paint INT,
+    Mods_Body INT,
+    Mods_Wrap INT,
+    Mods_Rims INT,
+    Mods_Interior INT,
+    Mods_Other INT,
+    Mods_ICE INT,
+    Mods_Aftermarket INT,
+    Mods_WIP INT,
+    Mods_Overall INT
+);
+.import data.csv Car_data
